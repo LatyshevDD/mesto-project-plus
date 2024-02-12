6237fd2f4c3319d29a280841c35a9a4813ecd584
Denis Latyshev <d.d.latyshev@yandex.ru>
2024-02-12 11:33:02 +0300

    feat: добавить в метод getUser ответы для ошибок 404 и 400

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,9 @@ import User from "../models/user";
 import { Request, Response, NextFunction } from 'express';
 import NotFoundError from "../errors/not-found-error-404";
 import NotCorrectDataError from "../errors/not-correct-data-400";
+import { ErrorsStatus } from "../types/types";
+import { ObjectId } from "bson";
+import mongoose, {Error as MongooseError } from "mongoose";
 
 export const getUsers = (req: Request, res: Response) => {
   User.find({})
@@ -9,16 +12,22 @@ export const getUsers = (req: Request, res: Response) => {
     .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }))
 };
 
-export const getUser = (req: Request, res: Response, next: NextFunction) => {
-  User.findById(req.params.userId)
-    .then(user => {
-      if (!user) {
-        throw new NotFoundError('Пользователь по указанному _id не найден.');
-      };
-
-      res.send({ data: user });
-    })
-    .catch(err => next(err));
+export const getUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).orFail(() => {
+      throw new NotFoundError('Пользователь по указанному _id не найден.')
+    });
+    res.status(ErrorsStatus.STATUS_OK).send(user);
+  } catch (error: any) {
+    if (error instanceof NotFoundError && error.message === "Пользователь по указанному _id не найден.") {
+      return next(error)
+    };
+    if(error instanceof MongooseError.CastError) {
+      const err = new NotCorrectDataError("Переданы некорректные данные при запросе информации о пользователе")
+      return next(err)
+    };
+  }
 };
 
 export const postUser = (req: Request, res: Response, next: NextFunction) => {
