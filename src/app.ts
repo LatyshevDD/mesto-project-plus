59faca01ce16d3416c239f4847a01abb878cf068
Denis Latyshev <d.d.latyshev@yandex.ru>
2024-02-13 13:51:24 +0300

    fix: добавить helmet, настроить 404 error для несуществующих маршрутов

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import express, { Request, Response, NextFunction} from 'express';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
 import { IError } from './types/types';
 import { IRequest } from './types/types';
 import userRouter from './routes/users';
 import cardRouter from './routes/cards';
 import { ErrorsStatus } from './types/types';
 import { errors } from 'celebrate';
+import NotFoundError from './errors/not-found-error-404';
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -26,7 +29,11 @@ app.use((req: IRequest, res: Response, next: NextFunction) => {
 
 app.use('/', userRouter);
 app.use('/', cardRouter);
-
+app.use('*', (req: Request, res: Response) => {
+  res
+    .status(ErrorsStatus.STATUS_NOT_FOUND)
+    .send({ message: 'Запрашиваемый ресурс не найден' });
+});
 app.use(errors());
 
 app.use((err: IError, req: Request, res: Response, next: NextFunction) => {
@@ -43,4 +50,4 @@ app.use((err: IError, req: Request, res: Response, next: NextFunction) => {
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`)
-})
+});
